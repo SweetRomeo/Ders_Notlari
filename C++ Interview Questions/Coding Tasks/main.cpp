5a5dd7a27230188d1761803388275ad0c41bf0c4
Berke Sertel <76247074+SweetRomeo@users.noreply.github.com>
2024-08-22 01:06:32 +0300

    Add files via upload
    Questions Marks question

--- C++ Interview Questions/Coding Tasks/main.cpp
+++ C++ Interview Questions/Coding Tasks/main.cpp
@@ -1,51 +1,50 @@
 /*----------------------------------------
-Longest Word
-Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string.
-If there are two or more words that are the same length, return the first word from the string with that length.
-Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
-Examples
-Input: "fun&!! time"
-Output: time
-Input: "I love dogs"
-Output: love
-Tags
+Questions Marks
+Have the function QuestionsMarks(str) take the str string
+parameter, which will contain single digit numbers, letters,
+and question marks, and check if there are exactly 3 question marks
+between every pair of two numbers that add up to 10. If so, then your
+program should return the string true, otherwise it should return the string false.
+If there aren't any two numbers that add up to 10 in the string, then your program should return false as well.
+
+For example: if str is "arrb6???4xxbl5???eee5" then your program should return true because
+there are exactly 3 question marks between 6 and 4, and 3 question marks between 5 and 5 at the end of the string.
 ---------------------------------------------*/
 #include <iostream>
 #include <string>
 #include <cctype>
+using namespace std;
 
-std::string LongestWord(const std::string &sen) {
-    std::string longestWord;
-    std::string currentWord;
-    size_t maxLength = 0;
+string QuestionsMarks(const string& str) {
+    int prev_num = -1;
+    int question_marks = 0;
+    bool found_pair = false;
 
-    for (char c : sen) {
-        if (std::isalnum(c)) {
-            currentWord += c;
-        } else {
-            if (currentWord.length() > maxLength) {
-                maxLength = currentWord.length();
-                longestWord = currentWord;
+    for (char c : str) {
+        if (isdigit(c)) {
+            int curr_num = c - '0';
+            if (prev_num != -1 && (prev_num + curr_num == 10)) {
+                if (question_marks == 3) {
+                    found_pair = true;
+                } else {
+                    return "false";
+                }
             }
-            currentWord = "";
+            prev_num = curr_num;
+            question_marks = 0;
+        } else if (c == '?') {
+            question_marks++;
         }
     }
 
-    if (currentWord.length() > maxLength) {
-        longestWord = currentWord;
-    }
-
-    return longestWord;
+    return found_pair ? "true" : "false";
 }
 
-int main() {
-    std::string sentence1 = "fun&!! time";
-    std::string sentence2 = "I love dogs";
-    std::string sentence3 = "Hello world123 567";
-
-    std::cout << "Longest word in '" << sentence1 << "' is: " << LongestWord(sentence1) << std::endl;
-    std::cout << "Longest word in '" << sentence2 << "' is: " << LongestWord(sentence2) << std::endl;
-    std::cout << "Longest word in '" << sentence3 << "' is: " << LongestWord(sentence3) << std::endl;
-
+int main()
+{
+    string input;
+    cout << "Enter the string: ";
+    cin >> input;
+    cout << QuestionsMarks(input) << endl;
     return 0;
 }
