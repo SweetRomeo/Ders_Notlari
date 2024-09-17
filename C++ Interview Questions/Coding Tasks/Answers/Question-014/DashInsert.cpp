544ff27b7b9d3e930dc5f0ff5d3a536bfbda768d
Berke Sertel <76247074+SweetRomeo@users.noreply.github.com>
2024-09-17 16:40:30 +0300

    Update DashInsert.cpp

--- C++ Interview Questions/Coding Tasks/Answers/Question-014/DashInsert.cpp
+++ C++ Interview Questions/Coding Tasks/Answers/Question-014/DashInsert.cpp
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------
-13-) Dash Insert
+14-) Dash Insert
 Have the function DashInsert(str) insert dashes ('-') between each two odd numbers in str. For example: if str is 454793 the output should be 4547-9-3. Don't count zero as an odd number.
 Examples
 Input: 99946
